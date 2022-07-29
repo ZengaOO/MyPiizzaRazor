07ab8406ddfac8fffa15acf90c3597f1011c0457
ZengaOO <108860031+ZengaOO@users.noreply.github.com>
2022-07-29 21:56:49 +0300

    10

--- MyPiizzaRazor/Pages/Pizza.cshtml.cs
+++ MyPiizzaRazor/Pages/Pizza.cshtml.cs
@@ -1,14 +1,80 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyPiizzaRazor.Models;
 
 namespace MyPiizzaRazor.Pages
 {
     public class PizzaModel : PageModel
     {
-        public PizzaModel Pizza { get; set; }
-        public float PizzaPrice { get; set; }
+        public List<PizzasModel> fakePizzaDB = new List<PizzasModel>()
+        {
+            new PizzasModel(){
+                ImageTitle="Margerita",
+                PizzaName="Margerita", 
+                BasePrice=2,
+                Tomatte=true,
+                Chese=true,
+                Margerita=true,
+                FinalPrice=4
+            },
+            new PizzasModel(){
+                ImageTitle="Peperoni",
+                PizzaName="Peperoni",
+                BasePrice=2,
+                Tomatte=true,
+                Chese=true,
+                Margerita=true,
+                FinalPrice=5
+            },
+            new PizzasModel(){
+                ImageTitle="Zucchini",
+                PizzaName="Zucchini",
+                BasePrice=2,
+                Tomatte=true,
+                Chese=true,
+                Margerita=true,
+                FinalPrice=7
+            },
+            new PizzasModel(){
+                ImageTitle="Tomatte",
+                PizzaName="Tomatte",
+                BasePrice=2,
+                Tomatte=true,
+                Chese=true,
+                Zucchini=true,
+                FinalPrice=8
+            },
+            new PizzasModel(){
+                ImageTitle="Pineapple",
+                PizzaName="Pineapple",
+                BasePrice=2,
+                Tomatte=true,
+                Chese=true,
+                Pineapple=true,
+                FinalPrice=9
+            },
+            new PizzasModel(){
+                ImageTitle="Peperoni",
+                PizzaName="Peperoni",
+                BasePrice=2,
+                Tomatte=true,
+                Chese=true,
+                Peperoni=true,
+                Margerita=true,
+                FinalPrice=12
+            },
+            new PizzasModel(){
+                ImageTitle="Chese",
+                PizzaName="Chese",
+                BasePrice=2,
+                Tomatte=true,
+                Chese=true,
+                Margerita=true,
+                FinalPrice=14
+            }
 
-       public void OnGet()
+        };
+        public void OnGet()
         {
         }
 
