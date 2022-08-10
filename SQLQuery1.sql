7060b9656febda2a4d3670af0b2d61c86edc164c
ZengaOO <108860031+ZengaOO@users.noreply.github.com>
2022-08-10 23:34:50 +0300

    1234

---
+++ SQLQuery1.sql
@@ -0,0 +1,55 @@
+﻿/*
+Скрипт развертывания для PizzaApp
+
+Этот код был создан программным средством.
+Изменения, внесенные в этот файл, могут привести к неверному выполнению кода и будут потеряны
+в случае его повторного формирования.
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "PizzaApp"
+:setvar DefaultFilePrefix "PizzaApp"
+:setvar DefaultDataPath "C:\Users\Notebook HP\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\"
+:setvar DefaultLogPath "C:\Users\Notebook HP\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\"
+
+GO
+:on error exit
+GO
+/*
+Проверьте режим SQLCMD и отключите выполнение скрипта, если режим SQLCMD не поддерживается.
+Чтобы повторно включить скрипт после включения режима SQLCMD выполните следующую инструкцию:
+SET NOEXEC OFF; 
+*/
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'Для успешного выполнения этого скрипта должен быть включен режим SQLCMD.';
+        SET NOEXEC ON;
+    END
+
+
+GO
+USE [$(DatabaseName)];
+
+
+GO
+PRINT N'Идет изменение Таблица [dbo].[PizzaOrders]…';
+
+
+GO
+ALTER TABLE [dbo].[PizzaOrders]
+    ADD [Position1] NVARCHAR (MAX) NULL;
+
+
+GO
+PRINT N'Обновление завершено.';
+
+
+GO
